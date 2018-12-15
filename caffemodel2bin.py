a3c77a85ce97067f2eadfa17aa4c5f79849fa792
jiangwx <jiangwx@shanghaitech.edu.cn>
2018-12-15 19:16:17 +0800

    add extract blob

--- caffemodel2bin.py
+++ caffemodel2bin.py
@@ -1,4 +1,3 @@
-
 import caffe
 import numpy as np
 from numpy import array
@@ -11,7 +10,7 @@ import argparse
 parser = argparse.ArgumentParser(description='caffemodel to binary file')
 parser.add_argument('--prototxt', type=str, default='', help='caffe prototxt')
 parser.add_argument('--caffemodel', type=str, default='', help='caffe caffemodel')
-parser.add_argument('--path', type=str, default='weight', help='path to save the binary file')
+parser.add_argument('--path', type=str, default='.', help='path to save the binary file')
 args = parser.parse_args()
 
 if not os.path.isfile(args.prototxt):
@@ -20,37 +19,50 @@ if not os.path.isfile(args.prototxt):
 if not os.path.isfile(args.caffemodel):
     print 'please specify caffemodel'
     exit()
-if not os.path.exists(args.path):
-    os.makedirs(args.path)
+if not os.path.exists(args.path+'/weight'):
+    os.makedirs(args.path+'/weight')
+if not os.path.exists(args.path+'/blob'):
+    os.makedirs(args.path+'/blob')
 
 caffe.set_device(0)
 caffe.set_mode_gpu()
 net = caffe.Net(args.prototxt, args.caffemodel, caffe.TEST)
+net.forward()
+layers = list(net.blobs)
 
-blob = list(net.blobs)
-for layer in blob:
+for layer in layers:
     if(layer.find('conv')!=-1):
         if(layer.find('bn')!=-1):
-            mn=open(args.path + '/'+layer[:layer.rfind('_')]+'.mn','w')
+            mn=open(args.path+'/weight/' +layer[:layer.rfind('_')]+'.mn','w')
             mean=net.params[layer][0].data
             mean.tofile(mn)
-            print layer + ' mean saved to ' + args.path + '/'+ layer[:layer.rfind('_')] +'.mn'
+            print layer + ' mean saved to ' + args.path+'/weight/'+ layer[:layer.rfind('_')] +'.mn'
             variance=net.params[layer][1].data
         elif(layer.find('scale')!=-1):
-            bs=open(args.path + '/'+layer[:layer.rfind('_')]+'.bs','w')
+            bs=open(args.path+'/weight/'+layer[:layer.rfind('_')]+'.bs','w')
             bias = net.params[layer][1].data
             bias.tofile(bs)
-            print layer + ' scale and bias saved to ' + args.path + '/'+ layer[:layer.rfind('_')] +'.bs'
+            print layer + ' scale and bias saved to ' + args.path+'/weight/'+ layer[:layer.rfind('_')] +'.bs'
             scale = net.params[layer][0].data
             variance=1/np.sqrt(variance)
             variance_scale=np.multiply(scale, variance)
-            vs=open(args.path + '/'+layer[:layer.rfind('_')]+".vs",'w')
+            vs=open(args.path+'/weight/'+layer[:layer.rfind('_')]+".vs",'w')
             variance_scale.tofile(vs)
-            print layer[:layer.rfind('_')] + ' scale x variance saved to ' + args.path + '/' + layer[:layer.rfind('_')] +'.vs'
+            print layer[:layer.rfind('_')] + ' scale x variance saved to ' + args.path+'/weight/' + layer[:layer.rfind('_')] +'.vs'
         elif(layer.find('relu')!=-1):
             print layer + ' skip'
         else:
-            wt=open(args.path + '/'+ layer +'.wt','w')
+            wt=open(args.path+'/weight/'+ layer +'.wt','w')
             weight = net.params[layer][0].data
             weight.tofile(wt)
-            print layer + ' weight saved to ' + args.path + '/'+ layer +'.wt'
+            print layer + ' weight saved to ' + args.path+'/weight/'+ layer +'.wt'
+
+for layer in layers:
+    if (layer.find('label')!=-1)|(layer.find('accuracy')!=-1)|(layer.find('loss')!=-1)|(layer.find('split')!=-1):
+        print 'skip ' + layer
+    else:
+        bb = open(args.path+'/blob/'+layer+'.bb','w')
+        blob = net.blobs[layer].data[0]
+        blob.tofile(bb)
+        print layer +' data saved to ' + args.path +'/blob/'+ layer +'.bb'
+
